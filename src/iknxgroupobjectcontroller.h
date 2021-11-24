1882f97e44ac5ff5a7980fc48a0998a68d970161
Roman Kochkin <roman.kochkin@wirenboard.ru>
2021-11-24 08:29:49 +0300

    Documented IGroupObject, IKnxGroupObjectController

--- src/iknxgroupobjectcontroller.h
+++ src/iknxgroupobjectcontroller.h
@@ -10,13 +10,23 @@
 
 namespace knx
 {
+    /// Class containing group objects and routing incoming KNX messages to group objects
     class IKnxGroupObjectController
     {
     public:
+        /// Add group object to controller
+        /// \param groupAddress group address associated with a group object
+        /// \param groupObject group object for management
+        /// \return The group object has been successfully added to the controller
         virtual bool AddGroupObject(const knx::TKnxGroupAddress& groupAddress,
                                     const std::shared_ptr<object::IGroupObject>& groupObject) = 0;
-        virtual bool RemoveGroupObject(const TKnxGroupAddress& address) = 0;
 
+        /// Remove group object from controller
+        /// \param groupAddress group address associated with a group object
+        /// \return The group object has been successfully removed from the controller
+        virtual bool RemoveGroupObject(const TKnxGroupAddress& groupAddress) = 0;
+
+        /// Virtual destructor
         virtual ~IKnxGroupObjectController() = default;
     };
 }
--- src/knxgroupobject/igroupobject.h
+++ src/knxgroupobject/igroupobject.h
@@ -9,6 +9,7 @@ namespace knx
 {
     namespace object
     {
+        /// Structure for communicating with a group object
         struct TGroupObjectTransaction
         {
             TKnxGroupAddress Address;
@@ -18,14 +19,21 @@ namespace knx
 
         using ISenderGroupObject = ISender<TGroupObjectTransaction>;
 
+        /// Group object for working with a KNX network
         class IGroupObject
         {
         public:
+            /// Notifying a group object of a message addressed to it
+            /// \param transaction Input message from the KNX network
             virtual void KnxNotify(const TGroupObjectTransaction& transaction) = 0;
 
+            /// Linking the group object to a KNX message transmitter in the network
+            /// \param groupAddress the group address to which messages will be sent.
+            /// \param sender sender object
             virtual void SetKnxSender(const knx::TKnxGroupAddress& groupAddress,
                                       std::shared_ptr<ISenderGroupObject> sender) = 0;
 
+            /// Virtual destructor
             virtual ~IGroupObject() = default;
         };
     }
