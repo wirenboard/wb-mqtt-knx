25d489b9fcd29412555bfacbe03661f348049b4a
Roman Kochkin <roman.kochkin@wirenboard.ru>
2021-12-09 13:33:40 +0300

    Added documentation for Observer and TickTimer

--- src/ticktimer.h
+++ src/ticktimer.h
@@ -7,16 +7,24 @@
 
 namespace knx
 {
+    /// Tick timer event
     enum class TTickTimerEvent
     {
-        TimeIsUp = 0
+        TimeIsUp = 0 ///< The time interval has ended
     };
 
+    /// Service for generating periodic events
     class TTickTimer: public TObserver<TTickTimerEvent>
     {
     public:
+        /// Set timer period
+        /// \param period time duration
         void SetTickPeriod(std::chrono::milliseconds period);
+
+        /// Start service
         void Start();
+
+        /// Stop service
         void Stop();
 
     private:
