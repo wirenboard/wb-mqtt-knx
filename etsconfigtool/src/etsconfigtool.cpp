0b049bd3ab85fbdff089ce635b256442f693aab7
Roman Kochkin <roman.kochkin@wirenboard.ru>
2022-06-02 20:55:52 +0400

    Added warning about missing datapoint type

--- etsconfigtool/src/etsconfigtool.cpp
+++ etsconfigtool/src/etsconfigtool.cpp
@@ -55,6 +55,8 @@ namespace
             if (dpts != nullptr) {
                 control.DatapointType = DatapointTypeExportToConfig(dpts);
             } else {
+                std::cout << "Warning: For '" << control.Id << "' missing datapoint type, assigned by default"
+                          << std::endl;
                 control.DatapointType = DatapointTypeExportToConfigDefault();
             }
             control.ReadOnly = DEFAULT_IS_CONTROL_READONLY;
